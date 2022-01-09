cf8a9ff2fd207b87496e887f741ddd5cf5e0fb95
Banjoko Azeez <banjokoazeez@banjokos-mbp.home>
2022-01-09 19:10:47 +0000

    * Add validations to product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,6 @@
 class Product < ApplicationRecord
+  validates :code, presence: true, uniqueness: { case_sensitive: false }, length: {minimum: 3, maximum: 10}
+  validates :name, presence: true, length: {minimum: 3, maximum: 20}
+  validates :price, numericality: { greater_than_or_equal_to: 0, less_than: BigDecimal(10**2) },
+                    format: { with: /\A\d{1,2}(\.\d{1,2})?\z/ }
 end
